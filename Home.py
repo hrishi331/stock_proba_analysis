345a295be151bb52b5396a2bb0420c1b2ceb388b
Hrishikesh Kulkarni <hrishikesh331@gmail.com>
2025-03-10 17:12:58 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -10,7 +10,7 @@ by user
 st.write(text_1)
 
 
-st.page_link(r"pages\query_2.py",label='**Query 2**')
+st.page_link(r"pages/query_2.py",label='**Query 2**')
 text_2 = """Find probability of selected parameter % change(e.g.Close,Open,...etc) 
 lying out of given band formed by lower and upper limit provided
 by user
@@ -18,14 +18,14 @@ by user
 st.write(text_2)
 
 
-st.page_link(r"pages\query_3.py",label='**Query 3**')
+st.page_link(r"pages/query_3.py",label='**Query 3**')
 text_3 = """Find probability of selected parameter % change(e.g.Close,Open,...etc) 
 lying above limit provided by user
 """
 st.write(text_3)
 
 
-st.page_link(r"pages\query_4.py",label='**Query 4**')
+st.page_link(r"pages/query_4.py",label='**Query 4**')
 text_4 = """Find probability of selected parameter % change(e.g.Close,Open,...etc) 
 lying below limit provided by user
 """
