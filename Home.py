c6d3d06fb15906b66a492743beb7e479618f0206
hrishikesh331 <hrishikesh331@gmail.com>
2025-06-12 20:08:20 +0530

    Created query buttons for redirect

--- Home.py
+++ Home.py
@@ -2,31 +2,34 @@ import streamlit as st
 
 st.title("Probabilistic analysis of selected stock".upper())
 
-st.page_link(r"pages/query_1.py",label='**Query 1**')
-text_1 = """Find probability of selected parameter % change(e.g.Close,Open,...etc) 
+text_1 = """1. Find probability of selected parameter % change(e.g.Close,Open,...etc) 
 lying within given band formed by lower and upper limit provided
-by user
+by user.
 """
 st.write(text_1)
+if st.button(label="Query1 >>"):
+    st.switch_page(page='pages/query_1.py')
 
-
-st.page_link(r"pages/query_2.py",label='**Query 2**')
-text_2 = """Find probability of selected parameter % change(e.g.Close,Open,...etc) 
+text_2 = """2. Find probability of selected parameter % change(e.g.Close,Open,...etc) 
 lying out of given band formed by lower and upper limit provided
-by user
+by user.
 """
 st.write(text_2)
+if st.button(label="Query2 >>"):
+    st.switch_page(page='pages/query_2.py')
 
 
-st.page_link(r"pages/query_3.py",label='**Query 3**')
-text_3 = """Find probability of selected parameter % change(e.g.Close,Open,...etc) 
-lying above limit provided by user
+text_3 = """3. Find probability of selected parameter % change(e.g.Close,Open,...etc) 
+lying above limit provided by user.
 """
 st.write(text_3)
+if st.button(label="Query3 >>"):
+    st.switch_page(page='pages/query_3.py')
 
 
-st.page_link(r"pages/query_4.py",label='**Query 4**')
-text_4 = """Find probability of selected parameter % change(e.g.Close,Open,...etc) 
-lying below limit provided by user
+text_4 = """4. Find probability of selected parameter % change(e.g.Close,Open,...etc) 
+lying below limit provided by user.
 """
 st.write(text_4)
+if st.button(label="Query4 >>"):
+    st.switch_page(page='pages/query_4.py')
